bb28208fa6057e1971ba448ae18214033a24af14
jonhayman <jon.hayman@red-gate.com>
2017-12-15 16:28:24 +0000

    Add a toggle to show or hide the description

--- app3/src/Products.js
+++ app3/src/Products.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react'
 import './Products.css'
 
 class Product extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {showDescription: false};
+
+        this.toggleDescription = this.toggleDescription.bind(this);
+    }
+
+    toggleDescription(){
+        console.log("Toggling description");
+        this.setState({showDescription: !this.state.showDescription})
+    }
+
     render(){
         return <div className='product'>
             <div className='details'>
-                <div className='name'>{this.props.product.name}</div>
-                <div className='desc'>{this.props.product.description}</div>
+                <div className='name'>{this.props.product.name}
+                <span onClick={this.toggleDescription}>{this.state.showDescription?'-':'+'}</span>
+                </div>
+                {this.state.showDescription && <div className='desc'>{this.props.product.description}</div>}
             </div>
             <div className='actions'>
                 <div className='remove' title='fix me' onClick={() => this.props.removeProduct(this.props.product)}>x</div>
