ca76c626727208355b15ee220dbb02e6552d422a
jonhayman <jon.hayman@red-gate.com>
2017-12-15 16:16:18 +0000

    Do filter box for kata 3

--- app3/src/App.js
+++ app3/src/App.js
@@ -4,14 +4,51 @@ import './App.css'
 import data from './data.js'
 import Products from './Products.js'
 
+class FilterForm extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {filterString: ''};
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event){
+    const val = event.target.value;
+    this.setState({filterString:val});
+    this.props.filterProducts(val);
+  }
+
+
+  render(){
+    return (
+      <form> 
+       <label>Product name: 
+          <input name="productName" value={this.state.filterString} onChange={this.handleChange} />
+       </label> 
+      </form>
+    )
+  }
+}
+
+
 class App extends Component {
 
   constructor(props){
     super(props)
     this.state= {products: data.products}
 
-    this.handleAddProduct = this.handleAddProduct.bind(this)
-    this.removeProduct = this.removeProduct.bind(this)
+    this.handleAddProduct = this.handleAddProduct.bind(this);
+    this.removeProduct = this.removeProduct.bind(this);
+    this.filterProducts = this.filterProducts.bind(this);
+  }
+
+  filterProducts(productString){
+    console.log(productString);
+    const products = this.state.products;
+
+    const filteredProducts = data.products.filter(product => product.name.includes(productString));
+    this.setState({products: filteredProducts});
+    console.log(filteredProducts.length);
   }
 
   handleAddProduct(event){
@@ -36,7 +73,9 @@ class App extends Component {
       <div className="App-header">
         <h2>Kata 3- Filter, show and hide objects</h2>
       </div>
-      <div className='filter-products'>Filter products here...</div>
+      <div className='filter-products'>
+      <FilterForm filterProducts={this.filterProducts}/>
+      </div>
       <div className='add-product'>
         <form onSubmit={this.handleAddProduct}>
           <label>product name:
