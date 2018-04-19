90e61a2ef1c85021eb5eebf28c13d0d1f5fb3e10
jonhayman <jon.hayman@red-gate.com>
2018-04-19 13:39:41 +0100

    Step 4

--- app-ts-1/src/App.tsx
+++ app-ts-1/src/App.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
 import { Component } from 'react';
 import './App.css';
+import { GetData } from './data';
+
 
 class App extends Component {
   render(): JSX.Element {
+    const data = GetData();
+    const display = data.map((product) => <li key={product.name}>{product.description}</li>);
     return (
       <div className='App'>
         <div className='App-header'>
@@ -12,11 +16,12 @@ class App extends Component {
         <p className='App-intro'>
           To get started change this text and then save to reload.
         </p>
-        <div className='products'>
-          ... your code here
-        </div>
-      </div>);
+        {display}
+      </div>
+      );
   }
+  
 }
+
 
 export default App;
